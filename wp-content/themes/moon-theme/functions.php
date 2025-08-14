8ddf79c6572643b138c24031c4c9fd99250b4ab3
Rudra Pandit <rudra613@example.com>
2025-08-14 16:09:12 +0530

    final commit

--- wp-content/themes/moon-theme/functions.php
+++ wp-content/themes/moon-theme/functions.php
@@ -2,3 +2,5 @@
 require_once get_template_directory().'/inc/assets.php';
 require_once get_template_directory().'/inc/theme-setup.php';
 //add_filter('woocommerce_rest_check_permissions', '__return_true', 10, 4);
+//echo "hello";
+//echo "how are you";
